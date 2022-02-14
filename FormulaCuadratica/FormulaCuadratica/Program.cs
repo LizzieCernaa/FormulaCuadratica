3ea8111c0563059753744a500fb34e53e4fca9d0
LizzieCernaa <98780118+LizzieCernaa@users.noreply.github.com>
2022-02-14 14:02:43 -0600

    Agregando datos

---
+++ FormulaCuadratica/FormulaCuadratica/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormulaCuadratica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Algoritmo que resulve la ecuacion cuadratica");
+
+            Console.WriteLine("Ingresa A");
+            int A = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingresa B");
+            int B = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingresa C");
+            int C = int.Parse(Console.ReadLine());
+
+
+            Console.ReadKey();
+        }
+    }
+}
